b4a0532561d0845c9b858c55f467aeb3b2354fb7
Sandro Soncini <sandrosoncini@hotmail.com>
2020-04-21 15:25:47 -0700

    added validations to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+    validates(:title, presence: true, uniqueness: true)
+    validates(
+        :description,
+        presence: { message: "must exist" },
+        length: { minimum: 10 }
+    )
+    validates(
+        :price,
+        numericality: { greater_than_or_equal_to: 0, allow_blank: true }
+    )
 end
