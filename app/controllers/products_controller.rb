512961c77aa5ed83927d7a37d07bf7c74c1b0f8a
Sandro Soncini <sandrosoncini@hotmail.com>
2020-04-22 16:22:14 -0700

    labs solution

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,10 +4,47 @@ class ProductsController < ApplicationController
     end
 
     def new
-        @products= Product.new
+        @product= Product.new
     end
 
     def create
+        @product = Product.new(params.require(:product).permit(:title, :description, :price)) 
+        if @product.save  
+            redirect_to products_path  
+        else 
+            render :new 
+	    end
     end
+
+    def show 
+        id = params[:id]
+        @product = Product.find(id)
+    end
+
+    def destroy
+        id = params[:id]
+        @product = Product.find(id)
+        @product.destroy
+        redirect_to products_path
+    end
+
+    def edit 
+        id = params[:id]
+        @product = Product.find(id)
+    end 
+
+    def update
+
+        id = params[:id]
+        @product = Product.find(id)
+
+        if @product.update(params.require(:product).permit(:title, :description,:price))
+          redirect_to product_path(@product)
+        else
+          render :edit
+        end
+    end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,15 @@ Rails.application.routes.draw do
 
 
   get("/products", to: "products#index")
-  get("/products/new", to: "products#new")
+
+  get("/products/new", to: "products#new", as: :new_product)
+  post("/products", to: "products#create" )
+
+  get("/products/:id", to: 'products#show', as: :product)
+  delete("products/:id", to: 'products#destroy')
+  get("products/:id/edit", to: 'products#edit', as: :edit_product)
+  patch("/products/:id", to: "products#update")
+  
+
+
 end
